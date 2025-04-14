929d77e0892640063b902fd79ddfe4ed05f325fc
BilelDounar <bileldounar@gmail.com>
2025-04-14 12:07:52 +0200

    ajout de useState

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -3,30 +3,47 @@ import "./App.css";
 import Box from "./assets/components/Box";
 
 function App() {
-    const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <div>
-                <h1 className="text-xl font-extrabold">Tic-Tac-Toe</h1>
-                <span>Ici un tic-tac-toe</span>
+    let caseList = [
+        { id: 1, statut: '' },
+        { id: 2, statut: '' },
+        { id: 3, statut: '' },
+        { id: 4, statut: '' },
+        { id: 5, statut: '' },
+        { id: 6, statut: '' },
+        { id: 7, statut: '' },
+        { id: 8, statut: '' },
+        { id: 9, statut: '' },
+    ];
 
-                <div className="my-10">
-                    <div className="flex flex-row">
-                    <Box param='cross'/><Box param='circle'/><Box param='cross'/>
-                    </div>
+    const [statut, setIcon] = useState('');
 
-                    <div className="flex flex-row">
-                    <Box param='cross'/><Box param='cross'/><Box param='cross'/>
-                    </div>
+    const changeStatut = () => {
+        setIcon('cross')
+    }
 
-                    <div className="flex flex-row">
-                    <Box param='cross'/><Box param='cross'/><Box param='cross'/>
-                    </div>
-                </div>
+}
+
+
+return (
+    <div>
+        <h1 className="text-xl font-extrabold">Tic-Tac-Toe</h1>
+        <span>Ici un tic-tac-toe</span>
+
+        <div className="my-10 w-72">
+            <div className="flex flex-row flex-wrap">
+
+                {caseList.map((caseGrille) => (
+                    <Box id={caseGrille.id} statut={caseGrille.statut} onClick={() => changeStatut} />
+                ))
+                }
+
             </div>
-        </>
-    );
+
+
+        </div>
+    </div>
+);
 }
 
 export default App;
--- tictactoe/src/assets/components/Box.jsx
+++ tictactoe/src/assets/components/Box.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const Box = ({param}) => {
+const Box = ({ id, statut }) => {
     return (
         <>
-            <div className="w-24 h-24 border-2 rounded-2xl text-4xl text-white flex justify-center items-center">
-            {param == 'cross' ? <i className="fa-solid fa-xmark"></i> : <i className="fa-regular fa-circle"></i>}
+            <div id={id} className="w-24 cursor-pointer h-24 border-2 rounded-2xl text-4xl text-white flex justify-center items-center">
+                {statut == 'cross' ? <i className="fa-solid fa-xmark"></i> : statut == 'circle' ? <i className="fa-regular fa-circle"></i> : ''}
                 {/* <i className="fa-solid fa-xmark"></i><i className="fa-regular fa-circle"></i> */}
             </div>
         </>
